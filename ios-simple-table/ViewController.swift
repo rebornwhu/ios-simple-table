bb43f27f8cfaa1ea5c986e94ed54610608717e12
rebornwhu <rebornwhu@gmail.com>
2015-10-08 22:30:16 -0700

    Save progress

--- ios-simple-table/ViewController.swift
+++ ios-simple-table/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let dwarves = ["Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey", "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili", "Oin", "Gloin", "Bifur", "Bofur", "Bombur"]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        cell!.textLabel!.text = dwarves[indexPath.row]
+        return cell!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
