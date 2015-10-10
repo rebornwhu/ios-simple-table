73fe08f3fa56d0c61701fa90e63568df67c60e2f
rebornwhu <rebornwhu@gmail.com>
2015-10-09 22:17:59 -0700

    Add selection and block for cell

--- ios-simple-table/ViewController.swift
+++ ios-simple-table/ViewController.swift
@@ -27,6 +27,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
         }
         
+        let image = UIImage(named: "star")
+        cell!.imageView?.image = image
+        let highlightedImage = UIImage(named: "star2")
+        cell!.imageView?.highlightedImage = highlightedImage
+        
+        if indexPath.row < 7 {
+            cell!.detailTextLabel?.text = "Mr Disney"
+        }
+        else {
+            cell!.detailTextLabel?.text = "Mr Tolkien"
+        }
+        
         cell!.textLabel!.text = dwarves[indexPath.row]
         return cell!
     }
@@ -41,6 +53,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+        return indexPath.row % 4
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if indexPath.row == 0 {
+            return nil
+        }
+        else {
+            return indexPath
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let rowValue = dwarves[indexPath.row]
+        let message = "You selected \(rowValue)"
+        
+        let controller = UIAlertController(title: "Row Selected", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Yes I Did", style: .Default, handler: nil)
+        controller.addAction(action)
+        
+        presentViewController(controller, animated: true, completion: nil)
+    }
 
 }
 
